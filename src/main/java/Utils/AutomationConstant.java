6b8e4cf7f2d65b968388341606184dbfd15534d1
mmilshah <mmilshah11@gmail.com>
2017-02-28 22:57:11 +0000

    Update AutomationConstant.java
    made temp changes

--- src/main/java/Utils/AutomationConstant.java
+++ src/main/java/Utils/AutomationConstant.java
@@ -1,11 +1,9 @@
 package Utils;
 
-/**
- * Created by VRINDA on 21/02/2017.
- */
+
 public class AutomationConstant {
 
     public static final String URL = "http://www.asos.com/";
     //public static final String BROWSER = "Firefox";
-
+    public static final string BROWSER = "Chrome";
 }
