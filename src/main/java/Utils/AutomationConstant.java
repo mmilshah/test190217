fcc2455f82bb4f11bd3b3a8394f973a23aff4cf2
mmilshah <mmilshah11@gmail.com>
2017-02-28 22:58:14 +0000

    Update AutomationConstant.java

--- src/main/java/Utils/AutomationConstant.java
+++ src/main/java/Utils/AutomationConstant.java
@@ -5,5 +5,5 @@ public class AutomationConstant {
 
     public static final String URL = "http://www.asos.com/";
     //public static final String BROWSER = "Firefox";
-    public static final string BROWSER = "Chrome";
+    
 }
